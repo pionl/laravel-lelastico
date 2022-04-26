0fddb840a0ad63af6c4e947b6b4be5e0ad3be3d8
Martin Kluska <martin@kluska.cz>
2022-04-26 14:32:25 +0200

    Update query builder: Add bottomLeft / topRight settings to geo bounding box query

--- rector.php
+++ rector.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -12,7 +14,7 @@ use Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (ContainerConfigurator $containerConfigurator, RectorConfig $rectorConfig): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
@@ -41,4 +43,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(BooleanInBooleanNotRuleFixerRector::class)->configure([
         BooleanInTernaryOperatorRuleFixerRector::TREAT_AS_NON_EMPTY => false,
     ]);
+
+    // Does not work with trait
+    $rectorConfig->skip([RestoreDefaultNullToNullableTypePropertyRector::class]);
 };
