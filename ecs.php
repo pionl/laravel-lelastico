2527175cf7bbb727f6da77439f2704c6cb1c3391
CIBOT (Martin Kluska) <martin+cibot@kluska.cz>
2026-09-16 11:24:25 +0200

    feat(compatibility): add Laravel 12 support

--- ecs.php
+++ ecs.php
@@ -3,23 +3,12 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
-
-return static function (ECSConfig $config): void {
-    $config->import(SetList::PSR_12);
-    $config->import(SetList::SYMPLIFY);
-    $config->import(SetList::COMMON);
-    $config->import(SetList::CLEAN_CODE);
-
-    $parameters = $config->parameters();
-
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::PATHS, [
+return ECSConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         // __DIR__ . '/tests',
         __DIR__ . '/config',
         __DIR__ . '/ecs.php',
-    ]);
-};
+    ])
+    ->withParallel()
+    ->withPreparedSets(psr12: true, common: true, cleanCode: true);
--- rector.php
+++ rector.php
@@ -3,42 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
-use Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (RectorConfig $config): void {
-    $config->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/config',
         __DIR__ . '/src',
-    ]);
-    $config->phpVersion(PhpVersion::PHP_80);
-    $config->importShortClasses();
-    $config->importNames();
-    $config->import(LevelSetList::UP_TO_PHP_80);
-    $config->import(SetList::CODE_QUALITY);
-
-    $config->rule(AddVoidReturnTypeWhereNoReturnRector::class);
-    $config->ruleWithConfiguration(
-        TypedPropertyRector::class,
-        [
-            TypedPropertyRector::INLINE_PUBLIC => true,
-        ]
-    );
-    $config->ruleWithConfiguration(
-        BooleanInBooleanNotRuleFixerRector::class,
-        [
-            BooleanInTernaryOperatorRuleFixerRector::TREAT_AS_NON_EMPTY => false,
-        ]
-    );
-
-    // Does not work with trait
-    $config->skip([RestoreDefaultNullToNullableTypePropertyRector::class]);
-};
+    ])
+    ->withPhpSets(php85: true)
+    ->withPreparedSets(codeQuality: true)
+    ->withImportNames();
